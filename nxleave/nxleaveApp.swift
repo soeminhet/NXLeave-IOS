3780a1a04a99613ba09cdd49c76973b6d1ed01cf
soeminhtet <soeminhet@gmail.com>
2024-01-22 20:46:40 +0630

    Initial Commit

---
+++ nxleave/nxleaveApp.swift
@@ -0,0 +1,17 @@
+//
+//  nxleaveApp.swift
+//  nxleave
+//
+//  Created by Soe Min Htet M1 MackBook Pro on 22/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct nxleaveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
